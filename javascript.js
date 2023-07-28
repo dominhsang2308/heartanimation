5581a1f0fb8ec5ae01a17a92db58e0c5ddf61503
SangDoMinh <sang.dominh@trainingsccvn.onmicrosoft.com>
2023-07-28 20:40:06 +0700

    first commit heart

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const conn = document.querySelector('.container')
+const shape1 = document.getElementsByClassName('.shape1')
+const shape2 = document.getElementsByClassName('.shape2')
+conn.addEventListener('click', () => {
+     alert('I Love You Babyboo')
+})
